d6ca6bcd9f652a98a96e50c311ad18e782cafd20
v1ztep <v1ztep@gmail.com>
2020-03-24 22:06:41 +0300

    Update parse_tululu_category.py

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -2,16 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-url_category = 'http://tululu.org/l55/'
+pattern = 'http://tululu.org/l55/{}/'
 
+for category_page in range(1, 11):
+    url_category = pattern.format(category_page)
 
-response = requests.get(url_category, allow_redirects=False)
-response.raise_for_status()
+    response = requests.get(url_category, allow_redirects=False)
+    response.raise_for_status()
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'lxml')
-    books = soup.find_all('div', class_='bookimage')
-    for book in books:
-        href = book.a['href']
-        url_book = urljoin(url_category, href)
-        print(url_book)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'lxml')
+        books = soup.find_all('div', class_='bookimage')
+        for book in books:
+            href = book.a['href']
+            url_book = urljoin(url_category, href)
+            print(url_book)
