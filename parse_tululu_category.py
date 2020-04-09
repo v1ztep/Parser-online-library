93559b9e8fa56648e762dc4c037de6c7eed4edef
v1ztep <v1ztep@gmail.com>
2020-04-09 17:19:24 +0300

    Сократил фильтрацию селектами и добавил аргументов

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,35 +1,76 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from tululu import download_txt, download_image
 import re
 import json
+import argparse
+from pathvalidate import sanitize_filepath
 
-pattern_category = 'http://tululu.org/l55/{}/'
-count_book = 0
+base_url = 'http://tululu.org/l55/{}'
+
+
+parser = argparse.ArgumentParser(
+    description='Парсер онлайн библиотеки tululu.org'
+)
+parser.add_argument('--start_page', type=int, default=701,
+                    help='Стартовая страница категории')
+
+parser.add_argument('--end_page', type=int, default=9999,
+                    help='Конечная страница категории(не включительно)')
+
+parser.add_argument('--dest_folder', type=str, default='',
+                    help='Путь к каталогу с результатами парсинга("folder/")')
+
+parser.add_argument('--json_path', type=str, default='',
+                    help='Путь к *.json файлу("folder/")')
+
+parser.add_argument('--skip_imgs', action='store_true',
+                    help='Не скачивать картинки')
+
+parser.add_argument('--skip_txt', action='store_true',
+                    help='Не скачивать книги')
+
+args = parser.parse_args()
+
+
+if args.start_page > 1:
+    count_book = (args.start_page-1) * 25
+else:
+    count_book = 0
+
 descriptions = []
 
-for category_page in range(1, 2):
-    url_category = pattern_category.format(category_page)
+for category_page in range(args.start_page, args.end_page):
+    category_url = base_url.format(category_page)
 
-    response = requests.get(url_category, allow_redirects=False)
-    response.raise_for_status()
+    response_category = requests.get(category_url, allow_redirects=False)
+    response_category.raise_for_status()
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'lxml')
-        books = soup.find('div', id='content').find_all('table', class_='d_book')
+    if response_category.status_code == 200:
+        soup_category = BeautifulSoup(response_category.text, 'lxml')
+        books = soup_category.select('#content .d_book')
+
         for book in books:
+            count_book += 1
             author, title = book.a['title'].split(' - ', maxsplit=1)
+            print(count_book)
 
             image_src = book.img['src']
-            url_image = urljoin(pattern_category, image_src)
+            url_image = urljoin(base_url, image_src)
             name_image = image_src.split('/')[-1]
-            image_path = download_image(url_image, name_image)
+            if args.skip_imgs:
+                image_path = ''
+            else:
+                image_path = download_image(url_image, name_image, folder=args.dest_folder)
 
             book_id = re.findall(r'\d+', book.a['href'])[0]
             url_txt = f'http://tululu.org/txt.php?id={book_id}'
-            count_book += 1
-            book_path = download_txt(url_txt, f"{count_book}.{title}")
+            if args.skip_txt:
+                book_path = ''
+            else:
+                book_path = download_txt(url_txt, f"{count_book}.{title}", folder=args.dest_folder)
 
             url_book = f'http://tululu.org/b{book_id}/'
             response_book = requests.get(url_book, allow_redirects=False)
@@ -37,14 +78,14 @@ for category_page in range(1, 2):
 
             comments = []
             genres = []
-            if response.status_code == 200:
-                soup = BeautifulSoup(response_book.text, 'lxml')
+            if response_book.status_code == 200:
+                soup_book = BeautifulSoup(response_book.text, 'lxml')
 
-                comments_soup = soup.find_all('div', class_='texts')
+                comments_soup = soup_book.select('.texts')
                 for comment in comments_soup:
-                    comments.append(comment.span.string)
+                    comments.append(comment.span.text)
 
-                genres_soup = soup.find('span', class_='d_book').find_all('a')
+                genres_soup = soup_book.select('span.d_book a')
                 for genre in genres_soup:
                     genres.append(genre.text)
 
@@ -57,13 +98,20 @@ for category_page in range(1, 2):
                 "comments": comments
             }
             descriptions.append(description)
+    else:
+        break
 
-with open("description.json", "w", encoding='utf8') as file:
+
+correct_folder = sanitize_filepath(args.dest_folder)
+correct_path = os.path.join(correct_folder, "description.json")
+
+if args.dest_folder:
+    os.makedirs(correct_folder, exist_ok=True)
+
+if args.json_path:
+    correct_folder = sanitize_filepath(args.json_path)
+    correct_path = os.path.join(correct_folder, "description.json")
+    os.makedirs(correct_folder, exist_ok=True)
+
+with open(correct_path, "w", encoding='utf8') as file:
     json.dump(descriptions, file, ensure_ascii=False, indent=4)
-
-# with open("description.json", "r", encoding='utf8') as my_file:
-#     description = json.load(my_file)
-#     print(description)
-
-
-
