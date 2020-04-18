d8e98cd24b1d19da9dc86b763bcea863ed14a7b8
v1ztep <v1ztep@gmail.com>
2020-04-18 17:15:59 +0300

    Добавил функцию get_hash_sum

--- tululu.py
+++ tululu.py
@@ -1,6 +1,14 @@
 import os
 import requests
 from pathvalidate import sanitize_filename, sanitize_filepath
+import hashlib
+
+
+def get_hash_sum(response):
+    if response.status_code == 200:
+        md5 = hashlib.md5(response.content)
+        return md5.hexdigest()
+    return
 
 
 def download_txt(url, filename, folder=''):
@@ -19,31 +27,33 @@ def download_txt(url, filename, folder=''):
     response_txt = requests.get(url, allow_redirects=False)
     response_txt.raise_for_status()
 
-    correct_filename = f"{sanitize_filename(filename)}.txt"
-    correct_folder = sanitize_filepath(folder + 'books/')
-    correct_path = os.path.join(correct_folder, correct_filename)
-
-    os.makedirs(correct_folder, exist_ok=True)
-
     if response_txt.status_code == 200:
+        correct_filename = f"{get_hash_sum(response_txt)}_{sanitize_filename(filename)}.txt"
+        correct_folder = sanitize_filepath(os.path.join(folder, 'books'))
+        correct_path = os.path.join(correct_folder, correct_filename)
+
+        os.makedirs(correct_folder, exist_ok=True)
+
         with open(correct_path, 'w', encoding='utf8') as file:
             file.write(response_txt.text)
 
-    return correct_path
+        return correct_path
+    return
 
 
 def download_image(url, filename, folder=''):
     response_image = requests.get(url, allow_redirects=False)
     response_image.raise_for_status()
 
-    correct_filename = sanitize_filename(filename)
-    correct_folder = sanitize_filepath(folder + 'images/')
-    correct_path = os.path.join(correct_folder, correct_filename)
-
-    os.makedirs(correct_folder, exist_ok=True)
-
     if response_image.status_code == 200:
+        correct_filename = f"{get_hash_sum(response_image)}_{sanitize_filename(filename)}"
+        correct_folder = sanitize_filepath(os.path.join(folder, 'images'))
+        correct_path = os.path.join(correct_folder, correct_filename)
+
+        os.makedirs(correct_folder, exist_ok=True)
+
         with open(correct_path, 'wb') as file:
             file.write(response_image.content)
 
-    return correct_path
+        return correct_path
+    return
