30e9e4379060b2e45e89bfff7153ee0dda29057f
v1ztep <v1ztep@gmail.com>
2020-04-19 12:26:18 +0300

    Сделал код более плоским

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -13,7 +13,7 @@ def get_args():
 
     parser.add_argument('--start_page', type=int, default=1,
                         help='Стартовая страница категории(включительно)')
-    parser.add_argument('--end_page', type=int, default=2,
+    parser.add_argument('--end_page', type=int, default=9999,
                         help='Конечная страница категории(не включительно)')
 
     parser.add_argument('--dest_folder', type=str, default='',
@@ -49,48 +49,49 @@ def main():
         response_category = requests.get(category_url, allow_redirects=False)
         response_category.raise_for_status()
 
-        if response_category.status_code == 200:
-            soup_category = BeautifulSoup(response_category.text, 'lxml')
-            books = soup_category.select('#content .d_book')
-
-            for book in books:
-                author, title = book.a['title'].split(' - ', maxsplit=1)
-
-                image_src = book.img['src']
-                url_image = urljoin(base_url, image_src)
-                name_image = image_src.split('/')[-1]
-                if args.skip_imgs:
-                    image_path = ''
-                else:
-                    image_path = download_image(url_image, name_image, folder=args.dest_folder)
-
-                book_id = re.findall(r'\d+', book.a['href'])[0]
-                url_txt = f'http://tululu.org/txt.php?id={book_id}'
-                if args.skip_txt:
-                    book_path = ''
-                else:
-                    book_path = download_txt(url_txt, title, folder=args.dest_folder)
-
-                url_book = f'http://tululu.org/b{book_id}/'
-                response_book = requests.get(url_book, allow_redirects=False)
-                response_book.raise_for_status()
-
-                if response_book.status_code == 200:
-                    soup_book = BeautifulSoup(response_book.text, 'lxml')
-                    comments = get_texts(soup_book.select('.texts .black'))
-                    genres = get_texts(soup_book.select('span.d_book a'))
-
-                description = {
-                    "title": title,
-                    "author": author,
-                    "image_path": image_path,
-                    "book_path": book_path,
-                    "genres": genres,
-                    "comments": comments
-                }
-                descriptions.append(description)
-        else:
+        if not response_category.status_code == 200:
             break
+
+        soup_category = BeautifulSoup(response_category.text, 'lxml')
+        books = soup_category.select('#content .d_book')
+
+        for book in books:
+            author, title = book.a['title'].split(' - ', maxsplit=1)
+
+            image_src = book.img['src']
+            url_image = urljoin(base_url, image_src)
+            name_image = image_src.split('/')[-1]
+            if args.skip_imgs:
+                image_path = ''
+            else:
+                image_path = download_image(url_image, name_image, folder=args.dest_folder)
+
+            book_id = re.findall(r'\d+', book.a['href'])[0]
+            url_txt = f'http://tululu.org/txt.php?id={book_id}'
+            if args.skip_txt:
+                book_path = ''
+            else:
+                book_path = download_txt(url_txt, title, folder=args.dest_folder)
+
+            url_book = f'http://tululu.org/b{book_id}/'
+            response_book = requests.get(url_book, allow_redirects=False)
+            response_book.raise_for_status()
+
+            if response_book.status_code == 200:
+                soup_book = BeautifulSoup(response_book.text, 'lxml')
+                comments = get_texts(soup_book.select('.texts .black'))
+                genres = get_texts(soup_book.select('span.d_book a'))
+
+            description = {
+                "title": title,
+                "author": author,
+                "image_path": image_path,
+                "book_path": book_path,
+                "genres": genres,
+                "comments": comments
+            }
+            descriptions.append(description)
+
 
     dest_path = os.path.join(args.dest_folder, "description.json")
 
