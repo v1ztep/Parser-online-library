9e5b872e01d079c91dd00c54775ac33e82957e48
Vladimir <v1ztep@gmail.com>
2020-10-05 15:10:47 +0300

    Избавился от лишних else к более плоскому коду

--- tululu.py
+++ tululu.py
@@ -28,38 +28,40 @@ def download_txt(url, filename, folder=None):
     txt_response.raise_for_status()
 
     if txt_response.status_code == 200:
-        correct_filename = f"{get_hash_sum(txt_response)}_{sanitize_filename(filename)}.txt"
-        if folder is not None:
-            correct_folder = sanitize_filepath(os.path.join(folder, 'books'))
-        else:
-            correct_folder = "books"
-        correct_path = os.path.join(correct_folder, correct_filename)
+        return
 
-        os.makedirs(correct_folder, exist_ok=True)
+    correct_filename = f"{get_hash_sum(txt_response)}_{sanitize_filename(filename)}.txt"
+    if folder is not None:
+        correct_folder = sanitize_filepath(os.path.join(folder, 'books'))
+    else:
+        correct_folder = "books"
+    correct_path = os.path.join(correct_folder, correct_filename)
 
-        with open(correct_path, 'w', encoding='utf8') as file:
-            file.write(txt_response.text)
+    os.makedirs(correct_folder, exist_ok=True)
+    with open(correct_path, 'w', encoding='utf8') as file:
+        file.write(txt_response.text)
 
-        return correct_path
-    return
+    return correct_path
+
 
 
 def download_image(url, filename, folder=None):
     image_response = requests.get(url, allow_redirects=False)
     image_response.raise_for_status()
 
-    if image_response.status_code == 200:
-        correct_filename = f"{get_hash_sum(image_response)}_{sanitize_filename(filename)}"
-        if folder is not None:
-            correct_folder = sanitize_filepath(os.path.join(folder, 'images'))
-        else:
-            correct_folder = "images"
-        correct_path = os.path.join(correct_folder, correct_filename)
+    if not image_response.status_code == 200:
+        return
 
-        os.makedirs(correct_folder, exist_ok=True)
+    correct_filename = f"{get_hash_sum(image_response)}_{sanitize_filename(filename)}"
+    if folder is not None:
+        correct_folder = sanitize_filepath(os.path.join(folder, 'images'))
+    else:
+        correct_folder = "images"
+    correct_path = os.path.join(correct_folder, correct_filename)
 
-        with open(correct_path, 'wb') as file:
-            file.write(image_response.content)
+    os.makedirs(correct_folder, exist_ok=True)
+    with open(correct_path, 'wb') as file:
+        file.write(image_response.content)
 
-        return correct_path
-    return
+    return correct_path
+
